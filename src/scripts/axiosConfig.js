c13608197c16411f6ed701b80c21bd110f2f8e42
Javier <notri80@gmail.com>
2024-07-27 18:39:47 +0200

    Add API base URL to axios instance

--- src/scripts/axiosConfig.js
+++ src/scripts/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const instance = axios.create({
 
-    baseURL: 'http://localhost:8080',
+    baseURL: 'https://baby-pets-adoption.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     },
